e48607f4d0bac613c2b65e1a393eb859b388f598
Phuc Tran <phuc.ps.89@gmail.com>
2016-01-27 19:51:47 +0700

    update phpsysinfo

--- app/controllers/AdviceController.php
+++ app/controllers/AdviceController.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\DB;
 
 class AdviceController extends BaseController{
 
+    const VC = 100;
+
     function index(){
         $adviceDetailTableName = AdviceDetail::getTableName();
         $adviceTableName = Advice::getTableName();
@@ -125,7 +127,12 @@ class AdviceController extends BaseController{
                     return !in_array($row->id_question, Question::$_rootQuestionID);
                 });
                 if($listAnsOfNotFactor->count() > 0){
-
+                    $arrAnsPoint = $this->scoreAnswer($listAnsOfNotFactor->lists('id_answer'));
+                    $arrAnsPoint = $this->interaction($arrAnsPoint);
+                    echo '<pre>';
+                    print_r($arrAnsPoint);
+                    echo '</pre>';
+                    exit;
                 }
                 else{
                     $question = Question::find(5);
@@ -156,5 +163,127 @@ class AdviceController extends BaseController{
         else{
 
         }
+    }
+
+    private function scoreAnswer($listAnswer){
+        $listAns = Answer::whereNotNull('id_attribute')->get();
+        $arrAnsPoint = [];
+        foreach($listAns as $ans){
+            $arrAnsPoint[$ans->id] = [
+                'point' => null,
+                'level' => null,
+                'interactions' => [],
+                'attribute' => $ans->id_attribute
+            ];
+        }
+        foreach($listAnswer as $ans){
+            $arrAnsPoint[$ans]['point'] = 100;
+            $arrAnsPoint[$ans]['level'] = 1;
+        }
+        return $arrAnsPoint;
+    }
+
+    private function interaction($listAnsPoint){
+        foreach($listAnsPoint as $idAns=>$ansPoint){
+            if($ansPoint['level']){
+                $listAnsPoint = $this->interactionLoop($listAnsPoint, $idAns, $ansPoint['point']);
+            }
+        }
+        $idAttrForNext = $this->getAttributeForNext($listAnsPoint);
+        echo '<pre>';
+        print_r($listAnsPoint);
+        echo '</pre>';
+    }
+
+    private function interactionLoop($listAnsPoint, $idAnswer, $point, $level = 2){
+        $interactions = Interaction::where('id_answer_from', $idAnswer)->get();
+        foreach($interactions as $in){
+            $idAnsTo = $in->id_answer_to;
+            $ansPointTo = &$listAnsPoint[$idAnsTo];
+            $hasUpdatePoint = false;
+            try {
+                if (count($ansPointTo['interactions']) == 0) {
+                    if (!$ansPointTo['level'] || $ansPointTo['level'] > $level) {
+                        jumpLevel:
+                        $ansPointTo['point'] = $point * $in->point;
+                        $ansPointTo['level'] = $level;
+                        $ansPointTo['interactions'] = [$point, $in->point];
+                        $hasUpdatePoint = true;
+                    }
+                } else {
+                    if ($ansPointTo['level'] == $level) {
+                        $ansPointTo['interactions'][] = [$point, $in->point];
+                        $sumPoint = 0;
+                        $sumInter = 0;
+                        foreach ($ansPointTo['interactions'] as $interPoint) {
+                            $sumPoint += $interPoint[0] * $interPoint[1];
+                            $sumInter += $interPoint[1];
+                        }
+                        $ansPointTo['point'] = $sumPoint / $sumInter;
+                        $hasUpdatePoint = true;
+                    } else if ($ansPointTo['level'] > $level) {
+                        goto jumpLevel;
+                    }
+                }
+            }
+            catch(Exception $e){
+
+            }
+            if($hasUpdatePoint) {
+                $listAnsPoint = $this->interactionLoop($listAnsPoint, $idAnsTo, $ansPointTo['point'], $level + 1);
+            }
+        }
+        return $listAnsPoint;
+    }
+
+    private function getAttributeForNext($listAnsPoint){
+        $getInteractionCount =  function($idAttribute) {
+            $interactionTable = Interaction::getTableName();
+            return Answer::where('id_attribute', $idAttribute)
+                ->join(
+                    $interactionTable,
+                    $interactionTable.'.id_answer_from', '=',
+                    Answer::getTableName().'.id'
+                )
+                ->count(
+                    DB::raw('DISTINCT '.$interactionTable.'.id_answer_to')
+                );
+        };
+
+        $tempScoreAttribute = [];
+        foreach($listAnsPoint as $ansPoint){
+            if (empty($tempScoreAttribute[$ansPoint['attribute']])) {
+                $tempScoreAttribute[$ansPoint['attribute']] = [
+                    'hasScore' => 0,
+                    'numAns' => 0,
+                    'levelMin' => self::VC,
+                    'numInter' => $getInteractionCount($ansPoint['attribute']),
+                    'id' => $ansPoint['attribute'],
+                ];
+            }
+            $tempScoreAttribute[$ansPoint['attribute']]['numAns']++;
+            if($ansPoint['point'] !== null){
+                $tempScoreAttribute[$ansPoint['attribute']]['hasScore']++;
+                $tempScoreAttribute[$ansPoint['attribute']]['levelMin'] =
+                    min($tempScoreAttribute[$ansPoint['attribute']]['levelMin'], $ansPoint['level']);
+            }
+        }
+
+        usort($tempScoreAttribute, function($a, $b) use ($getInteractionCount){
+            $conA = $a['numAns'] - $a['hasScore'];
+            $conB = $b['numAns'] - $b['hasScore'];
+
+            return $conB > $conA ||
+                ($conB == $conA && $b['numInter'] > $a['numInter']) ||
+                ($conB == $conA && $b['numInter'] == $a['numInter'] && $b['levelMin'] > $a['levelMin']);
+        });
+
+        foreach($tempScoreAttribute as &$a){
+            $a['numInter'] = $getInteractionCount($a['id']);
+        }
+        echo '<pre>';
+        print_r($tempScoreAttribute);
+        echo '</pre>';
+        exit;
     }
 }
